61049f9446523b22d4184b6034f4215be528d002
Annaaste <anna.dyberg@gmail.com>
2022-04-17 12:32:17 +0200

    added structure for textfiles

---
+++ texts/substitutes.js
@@ -0,0 +1,14 @@
+const SUBSTITUTES = [
+  {
+    image: '',
+    ingredient: '', 
+    substitute: '',
+  },
+  {
+    image: '',
+    ingredient: '', 
+    substitute: '',
+  },
+]
+
+export default SUBSTITUTES;
