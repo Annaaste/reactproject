e347930d2356a38a6c8a79edce26e66cc1e033c6
Annaaste <anna.dyberg@gmail.com>
2022-04-17 12:09:11 +0200

    added EarlierSearches component

---
+++ components/EarlierSearches/EarlierSearches.jsx
@@ -0,0 +1,10 @@
+import styles from './EarlierSearches.module.scss'
+
+const EarlierSearches = () => {
+  return (
+    //Byt ut till h2 när rätt storlek finns
+    <h4>Tidigare sökningar</h4>
+   );
+}
+ 
+export default EarlierSearches;
