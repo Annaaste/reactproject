538047ba6c2336ca0254f046e940e1884b138ecb
Sally Resch <sallyresch27@gmail.com>
2022-04-21 15:11:35 +0200

    changed the img logo to ingrii-logo in navbar

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -3,30 +3,30 @@ import Image from 'next/image'
 import styles from './Navbar.module.scss'
 
 const Navbar = () => {
-  return ( 
+  return (
     <nav className={styles.navbar}>
       <Link href="/">
         <a aria-label='Gå till startsidan'>
-          <img 
-            src="../images/logo.png" 
+          <img
+            src="../images/Ingrii Black 1.svg"
             className={styles.logo}
             //Berättar för screenreaders att loggan går till startsidan
             alt="Logga med namnet ingrii och en cirkel på slutet "
           />
-      </a>
+        </a>
       </Link>
       <div className={styles.mainMenu}>
         <Link href="/"><a className={styles.menuSubstitute}>Substitute </a></Link>
         <Link href="/convert"><a className={styles.menuConvert}> Conversion</a></Link>
       </div>
-      <Image 
+      <Image
         width={15}
         height={15}
         alt=""
         src="/images/icons/starIcon.svg"
       />
     </nav>
-   );
+  );
 }
- 
+
 export default Navbar;
