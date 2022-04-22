513eb1c57b38a171928f457516f43047b919e2af
Sally Resch <sallyresch27@gmail.com>
2022-04-22 08:03:35 +0200

    Added placeholder

--- components/SearchBar/SearchBar.jsx
+++ components/SearchBar/SearchBar.jsx
@@ -3,7 +3,7 @@ import styles from './SearchBar.module.scss'
 const SearchBar = () => {
   return (
     <div className={styles.searchBarContainer}>
-      <input type="text"></input>
+      <input type="text" placeholder='Write your ingredient'></input>
     </div>
   );
 }
