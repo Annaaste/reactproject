03862566be84f53025e1960901abf0f4541c334c
hansdavidsson <92034296+hansdavidsson@users.noreply.github.com>
2022-04-28 21:07:52 +0200

    MeasureSelect.jsx added

---
+++ pages/convert/MeasureSelect.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import SubSelect from './SubSelect'
+import measures from './measures'
+
+const MeasureSelect = ({measurement, setMeasurement, unit, setUnitA, setUnitB}) => {
+  const onSelectChange = (value) => {
+    changeUnit(value)
+    setMeasurement(value)
+  }
+
+  const changeUnit = (value) => {
+    let {a, b, c} = measures
+    value = value === "b" ? "dL" : value
+    value = value === "c" ? "g" : value
+
+    switch (value) {
+      case "a":
+        setUnitA(a.set1["?"])
+        setUnitB(a.set1["?"])
+        break
+      case "dL":
+        setUnitA(b.set1["Liters"])
+        setUnitB(b.set1["Deciliters"])
+        break
+      case "mL":
+        setUnitA(b.set2["LitersML"])
+        setUnitB(b.set2["Milliliters"])
+        break
+      case "g":
+        setUnitA(c.set1["Kilograms"])
+        setUnitB(c.set1["Grams"])
+        break
+      case "lbs":
+        setUnitA(c.set2["KilogramsLBS"])
+        setUnitB(c.set2["Pounds"])
+        break
+      default:
+        break
+    }
+  }
+
+  return (
+    <div>
+      <label htmlFor="measurements">Convert from: </label>
+        <select name="measurements" id="measurements" onChange={({target}) => onSelectChange(target.value)}>
+          <option value="a">?</option>
+          <option value="b">LITERS</option>
+          <option value="c">KILOGRAMS</option>
+        </select>
+        <div>
+        <label htmlFor="measurements">Convert to: </label>
+        {["b", "c"].includes(measurement)
+          ? <SubSelect measurement={measurement} changeUnit={changeUnit} unit={unit}/>
+          : null
+        }
+        </div>
+    </div>
+  )
+}
+
+export default MeasureSelect
