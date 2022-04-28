d07c5b89e70dcadf6aac8ea7b4f168b41d459790
Sally Resch <sallyresch27@gmail.com>
2022-04-28 19:51:07 +0200

    added searchbar and button

--- components/SearchBar/SearchBar.jsx
+++ components/SearchBar/SearchBar.jsx
@@ -1,11 +1,12 @@
 import styles from './SearchBar.module.scss'
 
 const SearchBar = () => {
-  return ( 
+  return (
     <div className={styles.searchBarContainer}>
-      <p>Här ligger sökrutan och sökknappen</p>
+      <input className="inputPlaceholder" type="text" placeholder='Write your ingredient'></input>
+      <button className="searchButton"><img src='../images/buttonIcon.svg'></img></button>
     </div>
-   );
+  );
 }
- 
+
 export default SearchBar;
