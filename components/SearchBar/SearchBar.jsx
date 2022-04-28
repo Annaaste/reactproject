5e840204b7b9d20d250d896098712d490d22dfb1
Sally Resch <sallyresch27@gmail.com>
2022-04-28 12:33:03 +0200

    added className to button

--- components/SearchBar/SearchBar.jsx
+++ components/SearchBar/SearchBar.jsx
@@ -3,8 +3,8 @@ import styles from './SearchBar.module.scss'
 const SearchBar = () => {
   return (
     <div className={styles.searchBarContainer}>
-      <input className="input-placeholder" type="text" placeholder='Write your ingredient'></input>
-      <button><img src='../images/buttonIcon.svg'></img></button>
+      <input className="inputPlaceholder" type="text" placeholder='Write your ingredient'></input>
+      <button className="searchButton"><img src='../images/buttonIcon.svg'></img></button>
     </div>
   );
 }
