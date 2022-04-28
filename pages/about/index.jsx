0e605316833fac30197b6816314fedc92b0fa2f2
Annaaste <anna.dyberg@gmail.com>
2022-04-28 09:37:56 +0200

    removed signs from text

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -15,19 +15,19 @@ const About = () => {
       <div className={styles.mediaQueries}>
         <h1 className={styles.testColor}>Testsida</h1>
         <p><strong>Här ser man hur man gör media queries.</strong></p>
-        <p>Det nu vanliga hälsningsordet "hej" var tidigare bara en 
+        <p>Det nu vanliga hälsningsordet hej var tidigare bara en 
           interjektion. Ordet började först användas som hälsning av 
           medlemmarna i Götiska förbundet under tidigt 1800-tal, som 
           ansåg att ordet var vikingalikt, och menade att vikingarna 
-          faktiskt kunde sagt så. Efter detta togs "hej" upp i 
+          faktiskt kunde sagt så. Efter detta togs hej upp i 
           studentkretsar i Uppsala på 1870-talet, för att bli allmänt 
           spritt först på 1930-talet.
         </p>
         <p>Det togs även upp av 
           ungsocialisterna i 1900-talets början. Anton Nilson skrev 1980: 
-          "Per Albin Hansson blev redaktör för det nya förbundets tidning 
-          Fram. Ofta sammanträffade vi, hälsade vårt 'Hej'. 
-          (Det var ungsocialisterna som tog upp det lilla nätta ordet 'hej' 
+          Per Albin Hansson blev redaktör för det nya förbundets tidning 
+          Fram. Ofta sammanträffade vi, hälsade vårt Hej. 
+          Det var ungsocialisterna som tog upp det lilla nätta ordet hej 
           efter Götiska förbundet, som låtit det falla i glömska, och genom 
           ungsocialisterna och ungdemokraterna bredde det lilla ordet ut sig 
           över hela landet och blev omsider ett allmänt hälsningsord.
