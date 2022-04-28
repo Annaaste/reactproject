66adbbd0fadf2661a5659aae9dd6da6297b91ed3
hansdavidsson <92034296+hansdavidsson@users.noreply.github.com>
2022-04-28 21:16:44 +0200

    Switch.jsx added

---
+++ pages/convert/Switch.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Switch = ({unitA, unitB, setUnitA, setUnitB, setOutput}) => {
+  const onSwitch = (event) => {
+    event.preventDefault()
+    setUnitA(unitB)
+    setUnitB(unitA)
+    setOutput("")
+  }
+
+  return (
+    <p> 
+      <div>
+      <button type="button" className="button" onClick={onSwitch}>
+        <i className="button"></i> switch
+      </button>
+      </div>
+    </p>
+  )
+}
+
+export default Switch
