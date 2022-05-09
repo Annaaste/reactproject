1b736add766065c1e310c0f6c375af7016831102
Sally Resch <sallyresch27@gmail.com>
2022-05-09 20:13:09 +0200

    added filtering to the searchbar

--- pages/substitute-result/index.jsx
+++ pages/substitute-result/index.jsx
@@ -9,11 +9,17 @@ const SubstituteResult = () => {
     <div className={styles.resultContainer}>
       <input placeholder="Enter ingredient" onChange={event => setQuery(event.target.value)} />
       {
-        Data.map((post, index) => (
-          <div className="box" key={post.id}>
-            <p>Ingredient: {post.ingredient}</p>
-            <p>Description: {post.description}</p>
-            <p>Substitute: {post.substitute}</p>
+        Data.filter(post => {
+          if (query === '') {
+            return post;
+          } else if (post.ingredient.toLowerCase().includes(query.toLowerCase())) {
+            return post;
+          }
+        }).map((post, index) => (
+          <div className="box" key={index}>
+            <p>{post.ingredient}</p>
+            <p>{post.description}</p>
+            <p>{post.substitute}</p>
           </div>
         ))
       }
