7dab1e141d36aa931ff052f7175d9bb9e3a51302
Sally Resch <sallyresch27@gmail.com>
2022-05-09 19:58:01 +0200

    mock DB in substitute result page

--- pages/substitute-result/index.jsx
+++ pages/substitute-result/index.jsx
@@ -1,17 +1,21 @@
 import styles from './substitute-result.module.scss'
-import SearchBar from '../../components/SearchBar/SearchBar';
+import Data from "/Users/Sengangarn/Documents/STI/ReactNy/reactproject/texts/substitutes.json";
+import { useState } from "react";
+
+
 const SubstituteResult = () => {
+  const [query, setQuery] = useState("")
   return (
     <div className={styles.resultContainer}>
-      <div className={styles.searchBarPlacement}>
-        <SearchBar />
-      </div>
-      <div className={styles.resultTextField}>
-        <h1>Ingredient name</h1>
-        <subHeading>Substitutes:</subHeading>
-        <li>bla lba lba</li>
-        <subHeading>Notes:</subHeading>
-      </div>
+      <input placeholder="Enter ingredient" onChange={event => setQuery(event.target.value)} />
+      {
+        Data.map((post, index) => (
+          <div className="box" key={post.id}>
+            <p>{post.ingredient}</p>
+            <p>{post.substitute}</p>
+          </div>
+        ))
+      }
     </div>
   );
 }
