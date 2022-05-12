17947b79baf7888acd80847bc672f4dc004b1df8
Sally Resch <sallyresch27@gmail.com>
2022-05-12 11:22:04 +0200

    removed links to data and such in substitute resul

--- pages/substitute-result/index.jsx
+++ pages/substitute-result/index.jsx
@@ -1,31 +1,11 @@
 import styles from './substitute-result.module.scss'
-import Data from "/Users/Sengangarn/Documents/STI/ReactNy/reactproject/texts/substitutes.json";
-import { useState } from "react";
 
 
 const SubstituteResult = () => {
-  const [query, setQuery] = useState("")
+
   return (
     <div className={styles.resultContainer}>
-      <input placeholder="Enter ingredient" onChange={event => setQuery(event.target.value)} />
-      {
-        Data.filter(post => {
-          if (query === '') {
-            return null;
-          } else if (post.ingredient.toLowerCase().includes(query.toLowerCase())) {
-            return post;
-          }
-        }).map((post, index) => (
-          <div className="box" key={index}>
-            <br />
-            <h2>{post.ingredient}</h2>
-            <br />
-            <p>{post.description}</p>
-            <br />
-            <p>{post.substitute}</p>
-          </div>
-        ))
-      }
+      <input placeholder="Enter ingredient" />
     </div>
   );
 }
