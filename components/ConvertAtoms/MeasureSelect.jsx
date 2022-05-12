337552e2ec674df95ede44a943081c9e373856df
hansdavidsson <92034296+hansdavidsson@users.noreply.github.com>
2022-05-12 19:11:25 +0200

    added units (non-funct.) + result fixed  (4 dec.)

--- components/ConvertAtoms/MeasureSelect.jsx
+++ components/ConvertAtoms/MeasureSelect.jsx
@@ -52,6 +52,31 @@ const MeasureSelect = ({measurement, setMeasurement, unit, setUnitA, setUnitB}) 
           <option value="a">?</option>
           <option value="b">LITERS</option>
           <option value="c">KILOGRAMS</option>
+          <option value="d">Cups US</option>
+          <option value="e">Cups Metric</option>
+          <option value="f">Cups Imperial</option>
+          <option value="g">Deciliters dL</option>
+          <option value="h">Fluid Ounces UK fl oz</option>
+          <option value="i">Fluid Ounces US fl oz</option>
+          <option value="j">Gallons UK gal</option>
+          <option value="k">Gallons US</option>
+          <option value="l">Grams g</option>
+          <option value="m">Kilograms kg</option>
+          <option value="n">Liters L</option>
+          <option value="o">Milliliters mL</option>
+          <option value="p">Ounces oz</option>
+          <option value="q">Pints UK pt</option>
+          <option value="r">Pints US pt</option>
+          <option value="s">Pounds lb</option>
+          <option value="t">Quarts UK qt</option>
+          <option value="u">Quarts US qt</option>
+          <option value="v">Tablespoons US</option>
+          <option value="w">Tablespoons Metric</option>
+          <option value="x">Tablespoons Imperial</option>
+          <option value="y">Teaspoons US</option>
+          <option value="z">Teaspoons Metric</option>
+          <option value="zz">Teaspoons Imperial</option>
+
         </select>
         </div>
 
