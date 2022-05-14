7098b36986f12621769085011605d50959baa26b
Annaaste <anna.dyberg@gmail.com>
2022-05-14 16:21:38 +0200

    started design on 404 and added to layout

--- pages/404.jsx
+++ pages/404.jsx
@@ -4,9 +4,9 @@ import styles from './404.module.scss'
 const PageNotFound = () => {
   return ( 
     <div className={styles.pageNotFound}>
-      <h1>Ojoj...</h1>
-      <p>Du har kommit fel för här finns ingen sida.</p>
-      <Link href="/"><a className={styles.goToHomePage}>Tillbaka till startsidan</a></Link>
+      <p>Ops! We are sorry, please try again...</p>
+      <p>404</p>
+      <Link href="/"><a className={styles.goToHomePage}>Go back to startpage</a></Link>
     </div>
    );
 }
