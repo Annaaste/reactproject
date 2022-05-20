0ba3751575bb08ebd99758e5d2b7d4d5f17de30f
hansdavidsson <92034296+hansdavidsson@users.noreply.github.com>
2022-05-20 10:18:02 +0200

    design fixes selection text

--- components/ConvertAtoms/Select.jsx
+++ components/ConvertAtoms/Select.jsx
@@ -1844,60 +1844,60 @@ const Select = () => {
    
     <input className={styles.IN} id={"IN"} /* value={0} */ type="number" min="0" step="any"/>
       <select className={styles.selectIN} id={"selectIN"} name="fromUnit">
-        <option value="a">Cups US</option>
-        <option value="b">Cups Metric</option>
-        <option value="c">Cups Imperial</option>
-        <option value="d">Deciliters dL</option>
-        <option value="e">Fluid Ounces UK fl oz</option>
-        <option value="f">Fluid Ounces US fl oz</option>
-        <option value="g">Gallons UK gal</option>
-        <option value="h">Gallons US</option>
-        <option value="i">Grams g</option>
-        <option value="j">Kilograms kg</option>
-        <option value="k">Liters L</option>
-        <option value="l">Milliliters mL</option>
-        <option value="m">Ounces oz</option>
-        <option value="n">Pints UK pt</option>
-        <option value="o">Pints US pt</option>
-        <option value="p">Pounds lb</option>
-        <option value="q">Quarts UK qt</option>
-        <option value="r">Quarts US qt</option>
-        <option value="s">Tablespoons US</option>
-        <option value="t">Tablespoons Metric</option>
-        <option value="u">Tablespoons Imperial</option>
-        <option value="v">Teaspoons US</option>
-        <option value="w">Teaspoons Metric</option>
-        <option value="x">Teaspoons Imperial</option>
+        <option value="a">Cups (US)</option>
+        <option value="b">Cups (Metric)</option>
+        <option value="c">Cups (Imperial)</option>
+        <option value="d">Deciliters (dL)</option>
+        <option value="e">Fluid Ounces (UK fl oz)</option>
+        <option value="f">Fluid Ounces (US fl oz)</option>
+        <option value="g">Gallons (UK gal)</option>
+        <option value="h">Gallons (US)</option>
+        <option value="i">Grams (g)</option>
+        <option value="j">Kilograms (kg)</option>
+        <option value="k">Liters (L)</option>
+        <option value="l">Milliliters (mL)</option>
+        <option value="m">Ounces (oz)</option>
+        <option value="n">Pints (UK pt)</option>
+        <option value="o">Pints (US pt)</option>
+        <option value="p">Pounds (lb)</option>
+        <option value="q">Quarts (UK qt)</option>
+        <option value="r">Quarts (US qt)</option>
+        <option value="s">Tablespoons (US)</option>
+        <option value="t">Tablespoons (Metric)</option>
+        <option value="u">Tablespoons (Imperial)</option>
+        <option value="v">Teaspoons (US)</option>
+        <option value="w">Teaspoons (Metric)</option>
+        <option value="x">Teaspoons (Imperial)</option>
       </select>
       
       
       
     <input className={styles.OUT} id={"OUT"} type="number" readOnly/>
       <select className={styles.selectOUT} id={"selectOUT"} name="fromUnit">
-        <option value="a">Cups US</option>
-        <option value="b">Cups Metric</option>
-        <option value="c">Cups Imperial</option>
-        <option value="d">Deciliters dL</option>
-        <option value="e">Fluid Ounces UK fl oz</option>
-        <option value="f">Fluid Ounces US fl oz</option>
-        <option value="g">Gallons UK gal</option>
-        <option value="h">Gallons US</option>
-        <option value="i">Grams g</option>
-        <option value="j">Kilograms kg</option>
-        <option value="k">Liters L</option>
-        <option value="l">Milliliters mL</option>
-        <option value="m">Ounces oz</option>
-        <option value="n">Pints UK pt</option>
-        <option value="o">Pints US pt</option>
-        <option value="p">Pounds lb</option>
-        <option value="q">Quarts UK qt</option>
-        <option value="r">Quarts US qt</option>
-        <option value="s">Tablespoons US</option>
-        <option value="t">Tablespoons Metric</option>
-        <option value="u">Tablespoons Imperial</option>
-        <option value="v">Teaspoons US</option>
-        <option value="w">Teaspoons Metric</option>
-        <option value="x">Teaspoons Imperial</option>
+        <option value="a">Cups (US)</option>
+        <option value="b">Cups (Metric)</option>
+        <option value="c">Cups (Imperial)</option>
+        <option value="d">Deciliters (dL)</option>
+        <option value="e">Fluid Ounces (UK fl oz)</option>
+        <option value="f">Fluid Ounces (US fl oz)</option>
+        <option value="g">Gallons (UK gal)</option>
+        <option value="h">Gallons (US)</option>
+        <option value="i">Grams (g)</option>
+        <option value="j">Kilograms (kg)</option>
+        <option value="k">Liters (L)</option>
+        <option value="l">Milliliters (mL)</option>
+        <option value="m">Ounces (oz)</option>
+        <option value="n">Pints (UK pt)</option>
+        <option value="o">Pints (US pt)</option>
+        <option value="p">Pounds (lb)</option>
+        <option value="q">Quarts (UK qt)</option>
+        <option value="r">Quarts (US qt)</option>
+        <option value="s">Tablespoons (US)</option>
+        <option value="t">Tablespoons (Metric)</option>
+        <option value="u">Tablespoons (Imperial)</option>
+        <option value="v">Teaspoons (US)</option>
+        <option value="w">Teaspoons (Metric)</option>
+        <option value="x">Teaspoons (Imperial)</option>
       </select>
         
       <button onMouseOver={button}> convert</button>
