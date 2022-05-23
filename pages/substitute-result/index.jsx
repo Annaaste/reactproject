f33262fefb434e3f09eedafd492e9297081335ff
Sally Resch <sallyresch27@gmail.com>
2022-05-23 10:30:37 +0200

    added result page text

--- pages/substitute-result/index.jsx
+++ pages/substitute-result/index.jsx
@@ -5,7 +5,9 @@ const SubstituteResult = () => {
 
   return (
     <div className={styles.resultContainer}>
+      <h1>result page</h1>
       <input placeholder="Enter ingredient" />
+
     </div>
   );
 }
