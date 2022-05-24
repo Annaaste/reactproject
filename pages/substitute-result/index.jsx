6f8eaa7d17322cb22f25bd919fb2b050ac22fa52
Sally Resch <sallyresch27@gmail.com>
2022-05-24 10:55:21 +0200

    button added

--- pages/substitute-result/index.jsx
+++ pages/substitute-result/index.jsx
@@ -7,7 +7,7 @@ const SubstituteResult = () => {
     <div className={styles.resultContainer}>
       <h1>result page</h1>
       <input placeholder="Enter ingredient" />
-
+      <button className={styles.buttonIcon}><img src='../images/icons/buttonIcon.svg'></img></button>
     </div>
   );
 }
